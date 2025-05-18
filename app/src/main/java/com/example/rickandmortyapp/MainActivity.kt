62a73f4ab038c8f2ee13be3bfddcfcd92c3f7c1f
alicanaydin <alican.aydin010@gmail.com>
2025-05-18 18:33:05 +0300

    Rick And Morty App

--- app/src/main/java/com/example/rickandmortyapp/MainActivity.kt
+++ app/src/main/java/com/example/rickandmortyapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.rickandmortyapp
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -8,12 +9,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.example.rickandmortyapp.pages.characters_description_page.CharacterDetailScreen
 import com.example.rickandmortyapp.ui.theme.RickAndMortyAppTheme
-
+import java.io.File
 
 
 class MainActivity : ComponentActivity() {
@@ -24,6 +27,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val appContext = LocalContext.current
+            LaunchedEffect(Unit) {
+                val rootDir = File(appContext.filesDir, "/Users/alicanaydin/Desktop/my_compose_projects/RickAndMortyApp/app/src/main/java/com/example/rickandmortyapp")
+                listDirectoryStructure(rootDir)
+            }
+
             val navController = rememberNavController()
             RickAndMortyAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) {
@@ -36,3 +45,19 @@ class MainActivity : ComponentActivity() {
 
 
 
+
+fun listDirectoryStructure(directory: File, indent: String = "") {
+    if (directory.exists() && directory.isDirectory) {
+        val files = directory.listFiles()
+        files?.forEach { file ->
+            if (file.isDirectory) {
+                println("$indent📁 ${file.name}/")
+                listDirectoryStructure(file, "$indent  ")
+            } else {
+                println("$indent📄 ${file.name}")
+            }
+        }
+    } else {
+        println("Belirtilen yol geçerli bir klasör değil.")
+    }
+}
